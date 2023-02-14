7044c9dcaebb05b6a8721e8572941d1718c5dfb1
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-02-14 18:45:00 +0200

    Modified hooks and create one useOutside

--- client-side/app/hooks/useActions.ts
+++ client-side/app/hooks/useActions.ts
@@ -2,13 +2,10 @@ import { useMemo } from 'react'
 import { useDispatch } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import { carouselSlice } from '@/store/carousel/carousel.slice'
-
 import { cartSlice } from '../store/cart/cart.slice'
 
 const rootAction = {
-	...cartSlice.actions,
-	...carouselSlice.actions
+	...cartSlice.actions
 }
 
 export const useActions = () => {
