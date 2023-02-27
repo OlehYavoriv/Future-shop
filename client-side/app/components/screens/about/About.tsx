7708b844343073f3c2f7c0be7703aaa1057a7f72
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-02-27 11:37:34 +0200

    About page

---
+++ client-side/app/components/screens/about/About.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import Layout from '@/layout/Layout'
+
+const About: FC = () => {
+	return (
+		<Layout title='About Us'>
+			<h2 className='section__title text-center mt-2'>About us</h2>
+		</Layout>
+	)
+}
+
+export default About
---
+++ client-side/pages/about.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+import About from '@/screens/about/About'
+
+const AboutPage: NextPage = () => {
+	return <About />
+}
+
+export default AboutPage
